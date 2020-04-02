053bf63377fc5c0d97bdde600939864d2d97f74b
DarwinAx <62819849+DarwinAx@users.noreply.github.com>
2020-04-02 17:09:57 -0500

    Update xhome.php

--- anime/xhome.php
+++ anime/xhome.php
@@ -26,7 +26,7 @@ header('Content-Type: text/html; charset=utf-8');
 <script type="text/javascript" src="//goraps.com/mobile_redir.php?section=General&pub=516552&ga=g&desktop=1"></script>
 -->
 
-<script type="text/javascript" src="//goraps.com/tup.php?section=zgtvtabup&pt=7&pub=516552&ga=g"></script>
+<script type="text/javascript" src="//goraps.com/tup.php?section=daTabUp&pt=7&pub=993114&ga=g"></script>
 
    <!--inicioscrpgalleria-->
 <script type="text/javascript" src="html5lightbox.js"></script>
